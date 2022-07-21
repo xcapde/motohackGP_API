72cfd4f813350dfc3958c95e8141ddb130695d79
ozknkrm <kerimozkan11@gmail.com>
2022-07-21 18:08:27 +0200

    description updated

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -21,7 +21,7 @@ INTO
   motos
   (brand, model, image, location, prod_Year, description, cc, hp, km, price, is_Favorite, is_Classic, is_Eco, seller_id)
 VALUES
-  ('Kawasaki', 'Model 1', 'https://www.motofichas.com/images/phocagallery/Kawasaki/z900-2022/02-kawasaki-z900-2022-estudio-verde.jpg', 'Oviedo', 2021,'Description by seller' , 1100, '120cv / 88kw', 20000, 8000.00, 1, 0, 0, 1);
+  ('Kawasaki', 'Z900', 'https://www.motofichas.com/images/phocagallery/Kawasaki/z900-2022/02-kawasaki-z900-2022-estudio-verde.jpg', 'Oviedo', 2021,'Motorcycle taken care of in detail, reason for sale is the purchase of another motorcycle. Always in garage. You can see and try whenever you want, ready to roll without spending any extra.' , 1100, '120cv / 88kw', 20000, 8000.00, 1, 0, 0, 1);
 
 
 INSERT
@@ -29,7 +29,7 @@ INTO
   motos
   (brand, model, image, location, prod_Year, description, cc, hp, km, price, is_Favorite, is_Classic, is_Eco, seller_id)
 VALUES
-  ('Honda', 'Model 2', 'https://s3-eu-west-1.amazonaws.com/boxrepsol-site/uploads/HONDA_CBR650R_2021.jpg', 'Madrid', 2018,'Description by seller' , 600, '60cv / 44kw', 45000, 6999.99, 0, 0, 0, 1);
+  ('Honda', 'CBR650R', 'https://s3-eu-west-1.amazonaws.com/boxrepsol-site/uploads/HONDA_CBR650R_2021.jpg', 'Madrid', 2018,'Single owner CBR 650 R, in perfect condition, fully serviced. Delivered with motorcycle cover. Better see and try.' , 600, '60cv / 44kw', 45000, 6999.99, 0, 0, 0, 1);
 
 
 INSERT
@@ -37,7 +37,7 @@ INTO
   motos
   (brand, model, image, location, prod_Year, description, cc, hp, km, price, is_Favorite, is_Classic, is_Eco, seller_id)
 VALUES
-  ('Vespa', 'GTS-125-ABS', 'https://www.soymotero.net/sites/default/files/2016-02/Vespa_PX_125.jpg', 'Sevilla', 2010,'Better than new!' , 250, '50cv / 25kw', 52500, 29999.99, 0, 0, 0, 1);
+  ('Vespa', 'GTS-125-ABS', 'https://www.soymotero.net/sites/default/files/2016-02/Vespa_PX_125.jpg', 'Sevilla', 2010,'It is in very good condition with luggage rack and works perfectly. Can be seen at any time. It is recently painted and very well maintained and treated.' , 250, '50cv / 25kw', 52500, 29999.99, 0, 0, 0, 1);
 
 
 INSERT
@@ -45,7 +45,7 @@ INTO
   motos
   (brand, model, image, location, prod_Year, description, cc, hp, km, price, is_Favorite, is_Classic, is_Eco, seller_id)
 VALUES
-  ('Seat', 'Mo', 'https://www.seat.co.uk/content/countries/gb/seat-website/en/mo/seat-mo-125/_jcr_content/par/continuereading/continuereadingimage.resizedViewPort.noscale.assetRootS.jpg', 'Barcelona', 2020,'Ecofriendly!' , 125, '12cv / 10kw', 1200, 3299.99, 0, 0, 1, 2);
+  ('Seat', 'Mo', 'https://www.seat.co.uk/content/countries/gb/seat-website/en/mo/seat-mo-125/_jcr_content/par/continuereading/continuereadingimage.resizedViewPort.noscale.assetRootS.jpg', 'Barcelona', 2020,'Ecofriendly!. Electric motorcycle equivalent to a 125. 30 km of autonomy, with three types of driving. Ideal for the city for only 3 euros a day.' , 125, '12cv / 10kw', 1200, 3299.99, 0, 0, 1, 2);
 
 
 INSERT
@@ -53,7 +53,7 @@ INTO
   motos
   (brand, model, image, location, prod_Year, description, cc, hp, km, price, is_Favorite, is_Classic, is_Eco, seller_id)
 VALUES
-  ('Ducati', 'Streetfighter-848', 'http://www.lerepairedesmotards.com/img/actu/2011/nouveaute/ducati-streetfighter-848-2012-cote_hd.jpg', 'Bilbao', 2012, 'Description by seller', 849, '132cv / 97kw', 11000, 12000, 0, 0, 0, 2);
+  ('Ducati', 'Streetfighter-848', 'http://www.lerepairedesmotards.com/img/actu/2011/nouveaute/ducati-streetfighter-848-2012-cote_hd.jpg', 'Bilbao', 2012, 'For sale Ducati Streetfighter 848. Selling due to change of motorcycle. Perfect condition, to do nothing for many kms. I do no accept changes.', 849, '132cv / 97kw', 11000, 12000, 0, 0, 0, 2);
 
 
 INSERT
@@ -61,7 +61,7 @@ INTO
   motos
   (brand, model, image, location, prod_Year, description, cc, hp, km, price, is_Favorite, is_Classic, is_Eco, seller_id)
 VALUES
-  ('Triumph', 'Legend TT', 'https://www.totalmotorcycle.com/wp-content/uploads/2016/12/2000-Triumph-LegendTTc.jpg', 'Santander', 2000 , 'Description by seller', 885, '70cv / 51kw', 22000, 3999.99, 0, 1, 0, 2);
+  ('Triumph', 'Legend TT', 'https://www.totalmotorcycle.com/wp-content/uploads/2016/12/2000-Triumph-LegendTTc.jpg', 'Santander', 2000 , 'Beautiful very well-kept motorcycle, with revisions in order and limitation for the A2. New tires.', 885, '70cv / 51kw', 22000, 3999.99, 0, 1, 0, 2);
 
 
 INSERT
@@ -69,11 +69,11 @@ INTO
   motos
     (brand, model, image, location, prod_Year, description, cc, hp, km, price, is_Favorite, is_Classic, is_Eco, seller_id)
 VALUES
-  ('Indian', 'Scout', 'https://img.remediosdigitales.com/8b4d57/1000_1000_indian_scout_2015_silver_smoke_20/1366_2000.jpg', 'Valencia', 2015, 'El asiento no es el de fabrica',  1133, '100cv / 73kw', 5000, 10000, 0, 0, 0, 2);
+  ('Indian', 'Scout', 'https://img.remediosdigitales.com/8b4d57/1000_1000_indian_scout_2015_silver_smoke_20/1366_2000.jpg', 'Valencia', 2015, 'It has six months of official warranty left. Review of the 8000 km passed. I give the passenger seat and original indian footrests.',  1133, '100cv / 73kw', 5000, 10000, 0, 0, 0, 2);
 
 INSERT
 INTO
   motos
     (brand, model, image, location, prod_Year, description, cc, hp, km, price, is_Favorite, is_Classic, is_Eco, seller_id)
 VALUES
-  ('Aprillia', 'Mana 850 GT', 'https://motos.espirituracer.com/archivos/2019/01/Aprilia-Mana-850-GT-ABS-1.jpg', 'Granada', 2010,'Repintada con otra tonalidad de rojo',  839, '76cv / 56KW', 12000, 9500, 0, 0, 0, 1);
+  ('Aprilia', 'Mana 850 GT', 'https://motos.espirituracer.com/archivos/2019/01/Aprilia-Mana-850-GT-ABS-1.jpg', 'Granada', 2010,'Impeccable, it has never given any mechanical problem. I sell to move out of the country. It is perfect for those who come from a scooter and want to continue with automatic transmission.',  839, '76cv / 56KW', 12000, 9500, 0, 0, 0, 1);
