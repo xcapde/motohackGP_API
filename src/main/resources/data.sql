32426566f19a9958666acc28771384be245eb4a4
Francisco <Gimli22@gmail.com>
2022-07-21 14:38:53 +0200

    changes in data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -23,6 +23,7 @@ INTO
 VALUES
   ('Kawasaki', 'Model 1', 'https://www.motofichas.com/images/phocagallery/Kawasaki/z900-2022/02-kawasaki-z900-2022-estudio-verde.jpg', 'Oviedo', 2021,'Description by seller' , 1100, '120cv / 88kw', 20000, 8000.00, 1, 0, 0, 1);
 
+
 INSERT
 INTO
   motos
@@ -30,12 +31,13 @@ INTO
 VALUES
   ('Honda', 'Model 2', 'https://s3-eu-west-1.amazonaws.com/boxrepsol-site/uploads/HONDA_CBR650R_2021.jpg', 'Madrid', 2018,'Description by seller' , 600, '60cv / 44kw', 45000, 6999.99, 0, 0, 0, 1);
 
+
 INSERT
 INTO
   motos
   (brand, model, image, location, prod_Year, description, cc, hp, km, price, is_Favorite, is_Classic, is_Eco, seller_id)
 VALUES
-  ('Vespa', 'GTS-125-ABS', 'https://www.soymotero.net/sites/default/files/2016-02/Vespa_PX_125.jpg', 'Sevilla', 2010,'Better than new!' , 250, '50 / 25kw', 52500, 29999.99, 0, 1, 0, 1);
+  ('Vespa', 'GTS-125-ABS', 'https://www.soymotero.net/sites/default/files/2016-02/Vespa_PX_125.jpg', 'Sevilla', 2010,'Better than new!' , 250, '50cv / 25kw', 52500, 29999.99, 0, 0, 0, 1);
 
 
 INSERT
@@ -43,4 +45,35 @@ INTO
   motos
   (brand, model, image, location, prod_Year, description, cc, hp, km, price, is_Favorite, is_Classic, is_Eco, seller_id)
 VALUES
-  ('Seat', 'Mo', 'https://www.seat.co.uk/content/countries/gb/seat-website/en/mo/seat-mo-125/_jcr_content/par/continuereading/continuereadingimage.resizedViewPort.noscale.assetRootS.jpg', 'Barcelona', 2020,'Ecofriendly!' , 125, '12 / 10kw', 1200, 3299.99, 0, 0, 1, 2);
+  ('Seat', 'Mo', 'https://www.seat.co.uk/content/countries/gb/seat-website/en/mo/seat-mo-125/_jcr_content/par/continuereading/continuereadingimage.resizedViewPort.noscale.assetRootS.jpg', 'Barcelona', 2020,'Ecofriendly!' , 125, '12cv / 10kw', 1200, 3299.99, 0, 0, 1, 2);
+
+
+INSERT
+INTO
+  motos
+  (brand, model, image, location, prod_Year, description, cc, hp, km, price, is_Favorite, is_Classic, is_Eco, seller_id)
+VALUES
+  ('Ducati', 'Streetfighter-848', 'http://www.lerepairedesmotards.com/img/actu/2011/nouveaute/ducati-streetfighter-848-2012-cote_hd.jpg', 'Bilbao', 2012, 'Description by seller', 849, '132cv / 97kw', 11000, 12000, 0, 0, 0, 2);
+
+
+INSERT
+INTO
+  motos
+  (brand, model, image, location, prod_Year, description, cc, hp, km, price, is_Favorite, is_Classic, is_Eco, seller_id)
+VALUES
+  ('Triumph', 'Legend TT', 'https://www.totalmotorcycle.com/wp-content/uploads/2016/12/2000-Triumph-LegendTTc.jpg', 'Santander', 2000 , 'Description by seller', 885, '70cv / 51kw', 22000, 3999.99, 0, 1, 0, 2);
+
+
+INSERT
+INTO
+  motos
+    (brand, model, image, location, prod_Year, description, cc, hp, km, price, is_Favorite, is_Classic, is_Eco, seller_id)
+VALUES
+  ('Indian', 'Scout', 'https://img.remediosdigitales.com/8b4d57/1000_1000_indian_scout_2015_silver_smoke_20/1366_2000.jpg', 'Valencia', 2015, 'El asiento no es el de fabrica',  1133, '100cv / 73kw', 5000, 10000, 0, 0, 0, 2);
+
+INSERT
+INTO
+  motos
+    (brand, model, image, location, prod_Year, description, cc, hp, km, price, is_Favorite, is_Classic, is_Eco, seller_id)
+VALUES
+  ('Aprillia', 'Mana 850 GT', 'https://motos.espirituracer.com/archivos/2019/01/Aprilia-Mana-850-GT-ABS-1.jpg', 'Granada', 2010,'Repintada con otra tonalidad de rojo',  839, '76cv / 56KW', 12000, 9500, 0, 0, 0, 1);
