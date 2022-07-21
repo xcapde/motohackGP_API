f1deff11f56aa4c4e6e3da108418c964e5a3d407
xcapde <xcapde@hotmail.com>
2022-07-21 15:03:54 +0200

    updating data and working in putMapping of moto

---
+++ src/main/java/com/MotoHack_GP/MotoHack_GP/dto/MotoRequestDto.java
@@ -0,0 +1,10 @@
+package com.MotoHack_GP.MotoHack_GP.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class MotoRequestDto {
+    private Boolean isFavorite;
+}
--- src/main/java/com/MotoHack_GP/MotoHack_GP/services/IMotoService.java
+++ src/main/java/com/MotoHack_GP/MotoHack_GP/services/IMotoService.java
@@ -1,5 +1,6 @@
 package com.MotoHack_GP.MotoHack_GP.services;
 
+import com.MotoHack_GP.MotoHack_GP.dto.MotoRequestDto;
 import com.MotoHack_GP.MotoHack_GP.models.Moto;
 
 import java.util.List;
@@ -8,4 +9,5 @@ public interface IMotoService {
     List<Moto> getAll();
     Moto getById(Long id);
     List<Moto> getBySearch(String search);
+    Moto update(MotoRequestDto motoDto, Long id);
 }
